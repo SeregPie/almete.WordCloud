00482ec18302d1bde6a918121cffa44e29b40df9
Sergej Sintschilin <seregpie@gmail.com>
2018-01-31 22:47:40 +0100

    +

--- src/members/createPlaceBitImageFunction.js
+++ src/members/createPlaceBitImageFunction.js
@@ -2,18 +2,22 @@ import getOccupiedBitImagePixels from './getOccupiedBitImagePixels';
 import findPixel from './findPixel';
 
 export default function(spaceAspect) {
-	let space = {};
+	let space;
 	return function(image, imageWidth, imageHeight) {
 		let occupiedImagePixels = getOccupiedBitImagePixels(image, imageWidth, imageHeight);
-		let startImageLeft = -Math.floor(imageWidth / 2);
-		let startImageTop = -Math.floor(imageHeight / 2);
-		let [imageLeft, imageTop] = findPixel(spaceAspect, [startImageLeft, startImageTop], ([imageLeft, imageTop]) => {
-			return occupiedImagePixels.every(([imagePixelLeft, imagePixelTop]) => {
-				let spacePixelLeft = imageLeft + imagePixelLeft;
-				let spacePixelTop = imageTop + imagePixelTop;
-				return space[`${spacePixelLeft}|${spacePixelTop}`];
+		let imageLeft = -Math.floor(imageWidth / 2);
+		let imageTop = -Math.floor(imageHeight / 2);
+		if (space) {
+			[imageLeft, imageTop] = findPixel(spaceAspect, [imageLeft, imageTop], ([imageLeft, imageTop]) => {
+				return occupiedImagePixels.every(([imagePixelLeft, imagePixelTop]) => {
+					let spacePixelLeft = imageLeft + imagePixelLeft;
+					let spacePixelTop = imageTop + imagePixelTop;
+					return space[`${spacePixelLeft}|${spacePixelTop}`];
+				});
 			});
-		});
+		} else {
+			space = {};
+		}
 		occupiedImagePixels.forEach(([imagePixelLeft, imagePixelTop]) => {
 			let spacePixelLeft = imageLeft + imagePixelLeft;
 			let spacePixelTop = imageTop + imagePixelTop;
