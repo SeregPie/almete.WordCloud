04ea09fea73319936e1c15884b0cfa3ddcb1778a
Sergej Sintschilin <seregpie@gmail.com>
2018-01-31 16:06:26 +0100

    +

--- src/members/BitSpace_get.js
+++ src/members/BitSpace_get.js
@@ -1,3 +1,14 @@
-export default function(space, x, y) {
-
+export default function(space, ...path) {
+	path.every(x => {
+		if (space) {
+			if (x < 0) {
+				space = space[1][-x];
+			} else {
+				space = space[0][x];
+			}
+			return true;
+		}
+		return false;
+	});
+	return space;
 }
--- src/members/BitSpace_set.js
+++ src/members/BitSpace_set.js
@@ -1,3 +1,42 @@
-export default function(space, x, y, bit) {
+export default function(space, left, top) {
+	path.forEach(x => {
+		if (space) {
+			if (x < 0) {
+				space = space[1][-x];
+			} else {
+				space = space[0][x];
+			}
+			return true;
+		} else {
+			// pass
+		}
+		return false;
+	});
+	{
+		let i;
+		if (left < 0) {
+			left = -left - 1;
+			i = 1;
+		} else {
+			i = 0;
+		}
+		if (space[i][left]) {
+			space = space[i][left];
+		} else {
+			space[i][left] = (space = createBitSpace());
+		}
+	}
+	{
+		let i;
+		if (top < 0) {
+			top = -1 - top;
+			i = 1;
+		} else {
+			i = 0;
+		}
+		space[i][top] = value;
+	}
 
+
+	return space;
 }
