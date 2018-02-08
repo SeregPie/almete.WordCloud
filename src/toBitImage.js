619b5b7a91c5abc6a0503a32dfe1141d9eb7bdeb
Sergej Sintschilin <seregpie@gmail.com>
2018-02-08 20:27:09 +0100

    +

--- src/toBitImage.js
+++ src/toBitImage.js
@@ -1,6 +1,6 @@
 export default function(image, imageWidth, imageHeight) {
 	let returns = new Uint8Array(imageWidth * imageHeight);
-	for (let i = 0, ii = image.length; i < ii; ++i) {
+	for (let i = 0, ii = returns.length; i < ii; ++i) {
 		returns[i] = image[i * 4 + 3] ? 1 : 0;
 	}
 	return returns;
